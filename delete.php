f310d6d7602137f039f1e1c4c427c5cbd3f71f5e
Azucena-GS <granadosazucena396@gmail.com>
2026-02-19 09:01:18 -0600

    Replace database connection with include statement

--- delete.php
+++ delete.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost","crud","1234","crud_php");
+include("conexion.php");
 $conexion->query("DELETE FROM usuarios WHERE id=".$_GET['id']);
 
 header("Location:index.php?ok=1");
