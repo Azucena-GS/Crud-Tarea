4c87dd570e2520fefeac50c030c0fe51da0210a2
Azucena-GS <granadosazucena396@gmail.com>
2026-02-19 09:00:42 -0600

    Update create.php

--- create.php
+++ create.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost","crud","1234","crud_php");
+include("conexion.php");
 
 $conexion->query("INSERT INTO usuarios(nombre,email,edad)
 VALUES('{$_POST['nombre']}','{$_POST['email']}','{$_POST['edad']}')");
